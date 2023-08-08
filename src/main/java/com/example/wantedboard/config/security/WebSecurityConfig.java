c424a25d497ff420759f3b133c120dc02a6d0d43
seongHyun-Min <steve6133@naver.com>
2023-08-08 21:33:55 +0900

    feat : WebSecurityConfig 추가(Bcrypt 암호화)

---
+++ src/main/java/com/example/wantedboard/config/security/WebSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.example.wantedboard.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .cors().and()
+                .authorizeRequests()
+                .antMatchers("/users").permitAll() // 인증 요청은 모두 허용
+                .anyRequest().authenticated(); // 그 외의 요청은 인증 필요
+    }
+}
